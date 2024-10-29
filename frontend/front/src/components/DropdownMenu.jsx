b498db2b6d9bdf36abf3d6e2e9b891a0fb66fcc0
4priltw91 <Romanticloudz@gmail.com>
2024-10-30 00:53:49 +0900

    미사용 파일삭제

--- frontend/front/src/components/DropdownMenu.jsx
+++ frontend/front/src/components/DropdownMenu.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import './Menu.css';
-import './BusinessButton.css';
 
 const dropdownVariants = {
   open: {
