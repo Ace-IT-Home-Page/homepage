63222ca33a9f5da99df0e6fa4d6008ca5219c68f
HwaJin9718 <165185641+HwaJin9718@users.noreply.github.com>
2024-10-31 03:47:04 +0900

    add - admin login function , remove - comments and unnecessary imports

--- backend/src/database/connection.py
+++ backend/src/database/connection.py
@@ -4,7 +4,6 @@ from sqlalchemy.orm import sessionmaker
 DATABASE_URL = "mysql+pymysql://root:aceit@127.0.0.1:3306/aceit?charset=utf8"
 
 engine = create_engine(DATABASE_URL, echo=True)
-# engine = create_engine("mysql+pymysql://[username]:[password]@[host]/[database_name]", echo=True)
 
 SessionFactory = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
