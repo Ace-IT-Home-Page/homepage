ce8577258fb1c41487cb6997836a5ef9ba5f3fe6
4priltw91 <155038345+4priltw91@users.noreply.github.com>
2025-02-12 18:02:10 +0900

    Update main.py

--- backend/src/main.py
+++ backend/src/main.py
@@ -4,7 +4,7 @@ from fastapi.staticfiles import StaticFiles
 
 from api import information, history, business_client, company_vision_values, business_area, inquiry, login ,download
 
-app = FastAPI(root_path="/api")
+app = FastAPI()
 
 # 모든 도메인에 대해 CORS 허용
 app.add_middleware(
@@ -26,7 +26,7 @@ app.include_router(company_vision_values.router, prefix="/api")
 app.include_router(business_area.router, prefix="/api")
 app.include_router(inquiry.router, prefix="/api")
 app.include_router(login.router, prefix="/api")
-app.include_router(download.router)
+app.include_router(download.router, prefix="/api")
 
 def checking():
     return {"connect" : "success"}
